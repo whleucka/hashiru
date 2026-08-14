817ac817bf155fc517e0d6f861f131dafb2f0c68
William Hleucka <william.hleucka@gmail.com>
2026-08-14 16:07:24 -0600

    fix: update system script

--- tools/update-system.sh
+++ tools/update-system.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
 # This is really, really dumb lol
+echo "╔══════════════════════════════════════════╗"
+echo "║                                          ║"
+echo "║          ⚡  SYSTEM UPDATE  ⚡           ║"
+echo "║                                          ║"
+echo "║         Updating system. Stand by.       ║"
+echo "║                                          ║"
+echo "╚══════════════════════════════════════════╝"
+echo
 yay -Syu --noconfirm
 paplay "/opt/hashiru/tools/assets/wow.mp3" &
 confetti
